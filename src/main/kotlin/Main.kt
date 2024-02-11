f989f071bcdf4c8b8b386be16b5655208eed9520
Ian Lum <ianlum314@gmail.com>
2024-02-11 16:30:02 -0500

    implement solution for project euler 83

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,46 @@
 package org.example
 
+import java.io.File
+
+/**
+ * Solve Project Euler problem 83 [https://projecteuler.net/problem=83]
+ * Find the least cost path when traversing a matrix of integers.
+ * Construct the matrix from a text file of comma separated numbers,
+ * then apply Dijkstra's algorithm.
+ *
+ * @param filePath The path to the text file of numbers
+ * @return The cost of the least cost path as a [Double]
+ */
+fun pathSum(filePath: String): Double {
+    val lines = File(filePath)
+        .readLines()
+        .map{ it.split(",")}
+    val g = WeightedDirectedGraph<Pair<Int, Int>>()
+    // Add vertices
+    for ((y, line) in lines.withIndex()) {
+        for ((x, _) in line.withIndex()) {
+            g.addVertex(Pair(x,y))
+        }
+    }
+    // Add edges
+    for ((y, line) in lines.withIndex()) {
+        for ((x, weight) in line.withIndex()) {
+            for (neighbor in arrayOf(
+                Pair(x+1, y),
+                Pair(x-1, y),
+                Pair(x, y+1),
+                Pair(x, y-1),
+            )) {
+                g.addEdge(neighbor, Pair(x,y), weight.toDouble())
+            }
+        }
+    }
+    return g.dijkstraCost(
+        Pair(0,0),
+        Pair(lines.size-1, lines.size-1)
+    )!! + lines[0][0].toDouble()
+}
 fun main() {
-    val g = WeightedDirectedGraph<Int>()
-    g.addVertex(1)
-    g.addVertex(2)
-    g.addVertex(3)
-    g.addVertex(4)
-    g.addVertex(5)
-    g.addVertex(6)
-    g.addVertex(7)
-    g.addVertex(8)
-    g.addVertex(9)
-    g.addEdge(1,2, 1.0)
-    g.addEdge(2,4, 7.0)
-    g.addEdge(1,3, 3.0)
-    g.addEdge(3,4, 3.0)
-    g.addEdge(4,5, 1.0)
-    g.addEdge(5,7, 7.0)
-    g.addEdge(4,6, 3.0)
-    g.addEdge(6,7, 3.0)
-    g.addEdge(8,9, 1.0)
-    val p = g.dijkstra(1, 7)
-    println("$p")
+    println("Test solution: ${pathSum("data/test.txt")}")
+    println("Solution: ${pathSum("data/matrix.txt")}")
 }
