f457d93b81060a68e3784332798f6119bc810544
Ian Lum <ianlum314@gmail.com>
2024-02-10 22:47:03 -0500

    write dijkstra unit tests

--- src/test/kotlin/WeightedDirectedGraphTest.kt
+++ src/test/kotlin/WeightedDirectedGraphTest.kt
@@ -63,4 +63,56 @@ class WeightedDirectedGraphTest {
         g.addVertex(2)
         assertEquals(setOf(1, 2), g.getVertices())
     }
+
+    @Test
+    fun dijkstraSingle() {
+        val g = WeightedDirectedGraph<Int>()
+        g.addVertex(1)
+        assertEquals(listOf(1), g.dijkstra(1,1))
+    }
+
+    @Test
+    fun dijkstraShort() {
+        val g = WeightedDirectedGraph<Int>()
+        g.addVertex(1)
+        g.addVertex(2)
+        g.addEdge(1,2,1.0)
+        assertEquals(listOf(1, 2), g.dijkstra(1,2))
+    }
+
+    @Test
+    fun dijkstraLong() {
+        val g = WeightedDirectedGraph<Int>()
+        g.addVertex(1)
+        g.addVertex(2)
+        g.addVertex(3)
+        g.addVertex(4)
+        g.addVertex(5)
+        g.addVertex(6)
+        g.addVertex(7)
+        g.addVertex(8)
+        g.addVertex(9)
+        g.addEdge(1,2, 1.0)
+        g.addEdge(2,4, 7.0)
+        g.addEdge(1,3, 3.0)
+        g.addEdge(3,4, 3.0)
+        g.addEdge(4,5, 1.0)
+        g.addEdge(5,7, 7.0)
+        g.addEdge(4,6, 3.0)
+        g.addEdge(6,7, 3.0)
+        assertEquals(listOf(1,3,4,6,7), g.dijkstra(1,7))
+    }
+
+    @Test
+    fun dijkstraNoPath() {
+        val g = WeightedDirectedGraph<Int>()
+        g.addVertex(1)
+        g.addVertex(2)
+        g.addVertex(3)
+        g.addVertex(4)
+        g.addEdge(1,2,1.0)
+        g.addEdge(3,4,1.0)
+        assertNull(g.dijkstra(1,3))
+        assertNull(g.dijkstra(1,4))
+    }
 }
